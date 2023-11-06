31f48773c114b9f80ae84188879f3a257e69ac14
Mathieu Etourneaud <mathieu.etourneaud@lapiscine.pro>
2023-11-06 12:38:58 +0100

    Add toggle + comments

--- Revision JS/main.js
+++ Revision JS/main.js
@@ -1,8 +1,20 @@
+// Selection de tout les elements qui ont la classe "card"
 const catCards = document.querySelectorAll(`.card`)
 
+// Boucle pour executer une fonction sur chaque card trouvé
 catCards.forEach((catCard) => {
+
+    // Ajout d'un évènement pour cliquer sur une card
     catCard.addEventListener(`click`, () => {
+
+        // Selection du bouton qui possede la classe "availability"
         const availability = catCard.querySelector(`.availability`)
-        availability.style.display = `block`
+        
+        // Ajout d'une boucle if pour afficher/masquer le bouton
+        if(availability.style.display == `block`) {
+            availability.style.display = `none`
+        } else {
+            availability.style.display = `block`
+        }
     })
 })
