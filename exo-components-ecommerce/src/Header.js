66b1f1fd4618b44e3f658680b4fd1adeee353870
Mathieu Etourneaud <mathieu.etourneaud@lapiscine.pro>
2023-11-08 10:39:46 +0100

    Add itemsInCart ? in Header.js

--- exo-components-ecommerce/src/Header.js
+++ exo-components-ecommerce/src/Header.js
@@ -1,18 +1,24 @@
 function Header () {
     const isUserLogged = false
+    const itemsInCart = 2
     return (
         <header>
             <h1>Super Ecommerce</h1>
             <nav>
                 <ul>
-                    <li>Mon</li>
-                    <li>Super</li>
-                    <li>Menu</li>
                     {isUserLogged ? (
                         <li>Mathieu</li>
                     ) : ( 
                         <li>Veuillez vous connecter</li>
                     )}
+                    <li>Home</li>
+                    <li>Products</li>
+                    {itemsInCart > 0 ? (
+                        <li>cart : {itemsInCart}</li>
+                    ) : ( 
+                        <li>Panier vide</li>
+                    )}
+                    
                 </ul>
             </nav>
         </header>
